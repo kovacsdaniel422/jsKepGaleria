112eb707bf869e48beba3ae35f05ce9d1d24f843
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-03-22 10:28:24 +0100

    yes

--- javascript.js
+++ javascript.js
@@ -1,93 +1,85 @@
 window.addEventListener("load", init);
-window.addEventListener("click", galeria);
 
 
 function ID(elem) {
     return document.getElementById(elem);
 }
-
 function CLASS(elem) {
     return document.getElementsByClassName(elem);
 }
+
 var kepeim = [{
-        eleres: "kepek/kep01.jpg",
-        cim: "Állatok",
-        leiras: "Kecskék",
+    eleres: "kepek/kep01.jpg",
+    cim: "Állatok",
+    leiras: "Kecskék",},
+    {
+    eleres: "kepek/kep02.jpg",
+    cim: "Japán fa",
+    leiras: "Szép sétány"
     },
     {
-        eleres: "kepek/kep02.jpg",
-        cim: "Japán fa",
-        leiras: "Szép sétány"
+    eleres: "kepek/kep03.jpg",
+    cim: "Sárga virágok",
+    leiras: "Szép mezőföld"
     },
     {
-        eleres: "kepek/kep03.jpg",
-        cim: "Sárga virágok",
-        leiras: "Szép mezőföld"
+    eleres: "kepek/kep04.jpg",
+    cim: "Tájkép",
+    leiras: "Fent a magasban!"
     },
     {
-        eleres: "kepek/kep04.jpg",
-        cim: "Tájkép",
-        leiras: "Fent a magasban!"
-    },
-    {
-        eleres: "kepek/kep05.jpg",
-        cim: "Az út mellet",
-        leiras: "Szép fehér virágok az út mellet"
-    }
-]
-
+    eleres: "kepek/kep05.jpg",
+    cim: "Az út mellet",
+    leiras: "Szép fehér virágok az út mellet"
+    }]
 var kepindex = 0;
-
-function galeria() {
+function galeria(){
     var txt = "";
     for (let index = 0; index < kepeim.length; index++) {
-        txt += '<div class="kep"> <img id="${index}" src="${kepeim[kepindex].eleres} '
-        " alt="
-        " ></div>';
-
+        txt +=`<div class="kep" ><img  id="${index}" src ="${kepeim[index].eleres}" alt="${kepeim[index].cim}"  ></div>`;
+        
     }
-    ID("inp").innerHTML = txt;
+    ID("inp").innerHTML=txt;
+    for (let index = 0; index < kepeim.length; index++) {
+        ID(index).addEventListener("click",kepkivalsztas)
+        CLASS("kep")[index].style.border="1px solid red";
+        CLASS("kep")[index].className+=" kepekformalasa";
+    }
+    
 }
-
+function kepkivalsztas(){
+    console.log(event.target.id);
+    kepindex= event.target.id;
+    megjelenit();
+}
 function init() {
+    megjelenit();
+    galeria();
     ID("balra").addEventListener("click", balra);
     ID("jobbra").addEventListener("click", jobbra);
 }
-
-function megjelenit(kepindex) {
+function megjelenit() {
     ID("kivalasztott").src = kepeim[kepindex].eleres;
     ID("kivalasztott").alt = kepeim[kepindex].cim;
 
 }
-
 function balra() {
-    if (kepindex <= 0) {
-        kepindex = kepeim.length - 1;
-    } else {
+    if(kepindex <= 0){
+        kepindex = kepeim.length-1;
+    }
+    else{
         kepindex -= 1;
     }
     console.log(kepindex);
     megjelenit(kepindex);
 }
-
 function jobbra() {
-    if (kepindex >= kepeim.length - 1) {
+    if(kepindex >= kepeim.length-1){
         kepindex = 0;
-    } else {
-        kepindex += 1;
+    }
+    else{
+        kepindex ++;
     }
     console.log(kepindex);
     megjelenit(kepindex)
-}
-
-function elsokepClick() {
-    let text = document.getElementById("elsokep").innerHTML;
-    document.getElementById("valtoz").innerHTML = text;
-    console.log(text)
-}
-
-function harkepClick() {
-    let text = document.getElementById("harmadikkep").innerHTML;
-    document.getElementById("valtoz").innerHTML = text;
-    console.log(text)
 }
